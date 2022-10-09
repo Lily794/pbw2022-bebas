06500872507bbeb2ca1153260df23a5a0bb9f70a
Lily794 <111202214099@mhs.dinus.ac.id>
2022-10-09 22:28:05 +0700

    third commit

--- minggu-4/form.php
+++ minggu-4/form.php
@@ -1,21 +1,35 @@
 <?php
+$nisn=$_POST['nisn'];
+$asalsekolah=$_POST['asalsekolah'];
+$tahunlulus=$_POST['tahunlulus'];
 $nama=$_POST['nama'];
+$nik=$_POST['nik'];
 $tempatlahir=$_POST['tempatlahir'];
 $tanggal=$_POST['tanggal'];
 $agama=$_POST['agama'];
+$jeniskelamin=$_POST['jeniskelamin'];
 $alamat=$_POST['alamat'];
+$emailaktif=$_POST['emailaktif'];
 $nomerhp=$_POST['nomerhp'];
-$jk=$_POST['jk'];
-$hobi=$_POST['hobi'];
-$foto=$_POST['foto'];
+$nomerkartukeluarga=$_POST['nomerkartukeluarga'];
+$namaayahkandung=$_POST['namaayahkandung'];
+$pekerjaan=$_POST['pekerjaan'];
+$namaibukandung=$_POST['namaibukandung'];
 
+echo "NISN : ".$nisn."<br>";
+echo "Asal Sekolah : ".$asalsekolah."<br>";
+echo "Tahun Lulus : ".$tahunlulus."<br>";
 echo "Nama : ".$nama."<br>";
+echo "NIK : ".$nik."<br>";
 echo "Tempat Lahir : ".$tempatlahir."<br>";
 echo "Tanggal Lahir : ".$tanggal."<br>";
 echo "Agama : ".$agama."<br>";
+echo "Jenis Kelamin : ".$jeniskelamin."<br>";
 echo "Alamat : ".$alamat."<br>";
+echo "Email Aktif : ".$emailaktif."<br>";
 echo "Nomor HP : ".$nomerhp."<br>";
-echo "Jenis Kelamin : ".$jk."<br>";
-echo "Hobi : ".$hobi."<br>";
-echo "File : ".$foto."<br>";
+echo "Nomer Kartu Keluarga : ".$nomerkartukeluarga."<br>";
+echo "Nama Ayah Kandung: ".$namaayahkandung."<br>";
+echo "Pekerjaan: ".$pekerjaan."<br>";
+echo "Nama Ibu Kandung: ".$namaibukandung."<br>";
 ?>
---
+++ minggu-5/minggu-4/form.php
@@ -0,0 +1,35 @@
+<?php
+$nisn=$_POST['nisn'];
+$asalsekolah=$_POST['asalsekolah'];
+$tahunlulus=$_POST['tahunlulus'];
+$nama=$_POST['nama'];
+$nik=$_POST['nik'];
+$tempatlahir=$_POST['tempatlahir'];
+$tanggal=$_POST['tanggal'];
+$agama=$_POST['agama'];
+$jeniskelamin=$_POST['jeniskelamin'];
+$alamat=$_POST['alamat'];
+$emailaktif=$_POST['emailaktif'];
+$nomerhp=$_POST['nomerhp'];
+$nomerkartukeluarga=$_POST['nomerkartukeluarga'];
+$namaayahkandung=$_POST['namaayahkandung'];
+$pekerjaan=$_POST['pekerjaan'];
+$namaibukandung=$_POST['namaibukandung'];
+
+echo "NISN : ".$nisn."<br>";
+echo "Asal Sekolah : ".$asalsekolah."<br>";
+echo "Tahun Lulus : ".$tahunlulus."<br>";
+echo "Nama : ".$nama."<br>";
+echo "NIK : ".$nik."<br>";
+echo "Tempat Lahir : ".$tempatlahir."<br>";
+echo "Tanggal Lahir : ".$tanggal."<br>";
+echo "Agama : ".$agama."<br>";
+echo "Jenis Kelamin : ".$jeniskelamin."<br>";
+echo "Alamat : ".$alamat."<br>";
+echo "Email Aktif : ".$emailaktif."<br>";
+echo "Nomor HP : ".$nomerhp."<br>";
+echo "Nomer Kartu Keluarga : ".$nomerkartukeluarga."<br>";
+echo "Nama Ayah Kandung: ".$namaayahkandung."<br>";
+echo "Pekerjaan: ".$pekerjaan."<br>";
+echo "Nama Ibu Kandung: ".$namaibukandung."<br>";
+?>
